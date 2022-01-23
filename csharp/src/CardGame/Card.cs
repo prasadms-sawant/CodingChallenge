cf88b5859ddbf05e5f8219469d58add05c74a9a6
Prasad Sawant <Prasad.Sawant@wolterskluwer.com>
2022-01-23 15:08:22 +0530

    Initial Code Commit for Card Game Challenge

---
+++ csharp/src/CardGame/Card.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.CardGame
+{
+    public class Card : ICard
+    {
+        public Card(int suit, int value)
+        {
+            this.Suit = (Suit)suit;
+            this.Value = (Value)value;  
+
+        }
+        public Suit Suit {
+            get {
+                return this.Suit;
+            }
+           private set {
+                this.Suit = value;
+            } 
+        }
+
+        public Value Value {
+            get {
+                return this.Value;
+            }
+            private set {
+                this.Value = value; 
+            }
+        }
+
+        public bool Equals(ICard other) => throw new NotImplementedException();
+    }
+}
---
+++ csharp/src/CardGame/PackOfCards.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CodingChallenge.CardGame
+{
+    public class PackOfCards : IPackOfCards
+    {
+        private static int _maxNumberOfCards = 52;
+        private ICard[] _cards = new ICard[_maxNumberOfCards];
+        private int _currentCardPosition = 0;
+        public PackOfCards()
+        {
+            InitializePackOfCards();
+        }
+
+        private void InitializePackOfCards()
+        {
+            int n = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    _cards[n++] = new Card(i, j);
+                    Count++;
+                }
+            }
+            _currentCardPosition=0;
+        }
+
+        public int Count { get; private set; }
+
+        public IEnumerator<ICard> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+                yield return _cards[i];
+        }
+
+        public void Shuffle()
+        {
+
+        }
+
+        public ICard TakeCardFromTopOfPack()
+        {
+            if (_currentCardPosition< _maxNumberOfCards)
+            {
+                return _cards[_currentCardPosition++];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
