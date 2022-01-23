2c2b974495d11c46ef91b462e23a8d08ec0d577d
Prasad Sawant <Prasad.Sawant@wolterskluwer.com>
2022-01-23 11:19:14 +0530

    Method: Reverse Class: StringUtilities Purpose: To reverse the given input string

--- csharp/src/ReversingString/StringUtilities.cs
+++ csharp/src/ReversingString/StringUtilities.cs
@@ -1,9 +1,27 @@
-﻿namespace CodingChallenge.ReversingString;
+﻿using System.Text;
+
+namespace CodingChallenge.ReversingString;
 
 public class StringUtilities
 {
+    /// <summary>
+    ///  This method reverse the given string and returns the reversed string as an output
+    /// </summary>
+    /// <param name="s"> it is a string input</param>
+    /// <returns>It returns the reversed string passed as an input</returns>
     public static string Reverse(string s)
     {
-        throw new NotImplementedException();
+        // Convert the input to char array to iterate over each character.
+        char[] inputStrArr = s.ToCharArray();
+        StringBuilder sbToHoldReversedStr = new StringBuilder(); 
+
+        for (int i = inputStrArr.Length - 1; i >0 ; i--)
+        {
+            // appends the character in string builder starting from the last element of the array initialized.
+            sbToHoldReversedStr.Append(inputStrArr[i]);
+        }
+        return sbToHoldReversedStr.ToString();
     }
+
+
 }
