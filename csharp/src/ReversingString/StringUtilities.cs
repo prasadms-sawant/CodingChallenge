cbd33191cd29d1988561cc812f4945f1b0b2fa0d
Prasad Sawant <Prasad.Sawant@wolterskluwer.com>
2022-01-23 12:11:01 +0530

    Adding optimized method to return the reversed string + the unit test for the same.

--- csharp/src/ReversingString/StringUtilities.cs
+++ csharp/src/ReversingString/StringUtilities.cs
@@ -27,5 +27,31 @@ public class StringUtilities
         return sbToHoldReversedStr.ToString();
     }
 
+    /// <summary>
+    /// This method also returns the input string in reverse manner.
+    /// This method executes faster as it only travers half of string as we are swapping the char in a loop itself.
+    /// </summary>
+    /// <param name="s">it is a string input</param>
+    /// <returns>It returns the reversed string passed as an input</returns>
+    public static string OptimizedReverse(string s) {
+
+        // Return blank if string is null or empty.
+        if (String.IsNullOrEmpty(s))
+            return "";
+
+        // Convert the input to char array to iterate over each character.
+        char[] inputArr = s.ToCharArray();
+        int strLength = inputArr.Length;
+
+        for (int i = 0, j = strLength -1 ; i < j; i++,j--)
+        {
+            char c = inputArr[i];
+            inputArr[i] = inputArr[j];
+            inputArr[j] = c;
+        }
+
+        return string.Join("",inputArr);
+    }
+
 
 }
--- csharp/tests/ReverseString.Tests/StringUtilitiesTests.cs
+++ csharp/tests/ReverseString.Tests/StringUtilitiesTests.cs
@@ -9,9 +9,21 @@ namespace CodingChallenge.ReversingString.Tests
         [TestCase("hello","olleh")]
         [TestCase("","")]
         [TestCase(null,"")]
+        [TestCase("#sdfsdd","ddsfds#")]
         public void ReverseStringTest(string input, string expectedOp)
         {
             string output = StringUtilities.Reverse(input);
+            Assert.AreEqual(expectedOp, output);
+        }
+
+        [Test]
+        [TestCase("hello", "olleh")]
+        [TestCase("", "")]
+        [TestCase(null, "")]
+        [TestCase("#sdfsdd", "ddsfds#")]
+        [TestCase("abcdef","fedcba")]
+        public void OptimizedReverseStringTest(string input, string expectedOp) {
+            string output = StringUtilities.OptimizedReverse(input);
             Assert.AreEqual(expectedOp, output);
         }
     }
